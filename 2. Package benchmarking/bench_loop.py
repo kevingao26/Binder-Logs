3c49d37f53cb2da715ac4aec607bf6132300515a
Kevin Gao <kevingao@Kevins-MacBook-Pro.local>
2021-06-16 16:46:26 -1000

    6/16 dependencies added

--- 2. Package benchmarking/bench_loop.py
+++ 2. Package benchmarking/bench_loop.py
@@ -2,18 +2,35 @@ import subprocess
 import pandas as pd
 import re
 import pickle
+from datetime import datetime
 
 with open("dep_list.txt", "rb") as fp:   # Unpickling
     dep_list = pickle.load(fp)
 
 timings = pd.DataFrame(columns=["library", "real", "user", "sys"])
+dependencies = pd.DataFrame(columns=["library", "dependencies"])
 
-start = 6
-end = 10
+start = 0
+end = 3
 set_num = 1
+
+timer_index = 1
+t_list = []
+a = datetime.now()
+
 for i in dep_list[start:end]:
 
-    rc = subprocess.call("./benchmarker.sh '%s'" % i, shell=True)
+    if set_num == 1:
+        rc = subprocess.call("./benchmarker.sh %s %s" % (i, 1), shell=True)
+        dep = open("dependencies.txt", "r").read()
+        try:
+            dep_groups = re.search(r"Requires: (.+) Required-by", dep).group(1).split(", ")
+        except:
+            dep_groups = []
+        dependencies = dependencies.append({"library": i, "dependencies": dep_groups}, ignore_index=True)
+
+    else:
+        rc = subprocess.call("./benchmarker.sh %s %s" % (i, 0), shell=True)
 
     timetaken = open("timetaken.txt", "r").read()
     time_groups = re.search(r"real\s([0-9]+)m([0-9]+.[0-9]+)s\suser\s([0-9]+)m([0-9]+.[0-9]+)s\ssys\s([0-9]+)m([0-9]+.[0-9]+)s", timetaken)
@@ -25,5 +42,15 @@ for i in dep_list[start:end]:
 
     timings = timings.append({"library": i, "real": real_t, "user": user_t, "sys": sys_t}, ignore_index=True)
 
+    if timer_index % 50 == 0:
+        t_list.append(str(timer_index) + " " + str(datetime.now() - a))
+    timer_index += 1
+
 print(timings)
 timings.to_csv("pip_benchmarks_" + str(start) + "_" + str(end - 1) + "_" + str(set_num) + ".csv")
+
+print(dependencies)
+dependencies.to_csv("pip_dependencies_" + str(start) + "_" + str(end - 1) + "_" + str(set_num) + ".csv")
+
+print("t_list")
+print(t_list)
--- 2. Package benchmarking/benchmarker.sh
+++ 2. Package benchmarking/benchmarker.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 lib=$1
+round1=$2
 
 # make and enter virtual-environment folder
 mkdir ./ve
@@ -15,6 +16,12 @@ source randomenv/bin/activate
 # benchmarking
 exec 3>&1 4>&2
 timetaken=$( { time pip install $lib --no-cache-dir 1>&3 2>&4; } 2>&1 )
+
+if [[ $round1 -gt 0 ]]
+then
+  dependencies=$(python -m pip show $lib)
+fi
+
 exec 3>&- 4>&-
 
 # deactivate the virtual environment
@@ -28,5 +35,11 @@ rm -r ve
 rm -r .cache
 
 echo $timetaken > timetaken.txt
+
+if [[ $round1 -gt 0 ]]
+then
+  echo $dependencies > dependencies.txt
+fi
+
 exit
 
